d5adef123957dde11d7b77fcd2d40059ee90f089
Thibaut Jarosz <thibaut@jarosz.io>
2021-11-15 01:20:03 +0100

    Add MemorySymbolsApp

---
+++ Memory Symbols/MemorySymbolsApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct MemorySymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                DeckSelectionView()
+            }
+        }
+    }
+}
