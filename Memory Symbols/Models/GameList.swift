3f5077e47b2f5c8dfa48ed4636e9a43b416b5342
Thibaut Jarosz <thibaut@jarosz.io>
2021-11-26 00:44:28 +0100

    Update GameList when a Game changes

--- Memory Symbols/Models/GameList.swift
+++ Memory Symbols/Models/GameList.swift
@@ -5,6 +5,9 @@ class GameList: ObservableObject {
     /// The list of games, publicly available via subscript
     @Published private var games: [Deck:Game] = [:]
     
+    /// List of cancellable sinks
+    private var cancellables: [Deck:AnyCancellable] = [:]
+    
     /// Subscript to access the games
     subscript(deck: Deck) -> Game? {
         get {
@@ -12,6 +15,13 @@ class GameList: ObservableObject {
         }
         set {
             games[deck] = newValue
+            cancellables[deck]?.cancel()
+            
+            if let newValue = newValue {
+                cancellables[deck] = newValue.objectWillChange.sink { [weak self] _ in
+                    self?.objectWillChange.send()
+                }
+            }
         }
     }
 }
